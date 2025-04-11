441857315ec5f84c3edf8a1c1074e7237a89358b
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2025-04-11 15:25:33 +0300

    refactor message

--- functions/postMessageBuilder.js
+++ functions/postMessageBuilder.js
@@ -6,6 +6,6 @@ export default function postMessageBuilder({title, price, article, mark, product
           `➡️ <a href="${link}">Опис товару на сайті</a> ⬅️\n\n` +
           `Для запитань:\n` +
           `📞 <a href="tel:+380442470786">+380442470786</a>\n` +
-          `✉️ <a href="https://jysk.ua/customer-service-category/13152?question=ka07T000000c2JBQAY#service-category-ka07T000000c2JBQAY"><u>Написати лист</u></a>\n\n` +
+          `✉️ <a href="https://jysk.ua/customer-service-category/13152?question=ka07T000000c2JBQAY#service-category-ka07T000000c2JBQAY">Написати лист</a>\n\n` +
           `${hashtags}\n#JYSK #Outlet #Знижка `
 }
